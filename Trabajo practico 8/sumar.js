2f7f590620d1752b7894dbaf8817da9a297e11f9
Valentino Bellitto <163890897+VainillaSheep@users.noreply.github.com>
2024-11-07 21:10:11 -0300

    Tema: Javascript

---
+++ Trabajo practico 8/sumar.js
@@ -0,0 +1,8 @@
+let num1, num2, resultado;
+
+num1 = prompt("Ingresa el valor de num1:");
+num2 = prompt("Ingresa el valor de num2:");
+
+resultado = parseFloat(num1) + parseFloat(num2);
+
+console.log(`El resultado de sumar ${num1} y ${num2} es: ${resultado}`);
---
+++ Trabajo practico 8/viaje.js
@@ -0,0 +1,8 @@
+let diasViaje = 10;
+let presupuestoMaximo = 5000;
+let comida;
+let totalComidas = 40; 
+
+comida = presupuestoMaximo / totalComidas;
+
+alert(`Podés gastar $${comida} en cada comida para que te alcance la plata durante los ${diasViaje} días de viaje.`);
