b78c1ed56352c23ae8bb065af5f43499aa01b03b
Valentino Bellitto <163890897+VainillaSheep@users.noreply.github.com>
2024-11-07 21:19:35 -0300

    Tema: Devolucion de funciones y matematicas simples

---
+++ Trabajo Practico 9/ejercicio1.js
@@ -0,0 +1,23 @@
+function triplicador(numero) {
+    return numero * 3;
+}
+
+function multiplicador(num1, num2) {
+    return num1 * num2;
+}
+
+function division(num1, num2) {
+    return num1 / num2;
+}
+
+function resto(num1, num2) {
+    return num1 % num2;
+}
+
+const triplicar5 = 5 * 3;
+const multiplicarPor12 = triplicar5 * 12;
+const divisionPor12 = multiplicarPor12 / 12;
+const restoDivisionPor3 = divisionPor12 % 3;
+
+
+console.log(restoDivisionPor3); 
